af7bf92c5c0539c0d2c96be6bdc28b9dcbcffb16
qkrwogk <pjha999@naver.com>
2023-10-25 22:08:13 +0900

    [FEAT] Board Entity, Repository 구현 및 등록

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './boards.model';
+
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: BoardStatus.PUBLIC })
+  status: BoardStatus;
+}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+// import { Board } from './board.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
