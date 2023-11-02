28238dfd0cccc31384fc101cca7eab2b0b2a45f7
qkrwogk <pjha999@naver.com>
2023-11-02 18:03:15 +0900

    [FEAT] username에 UNIQUE 속성 주기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-// import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -15,7 +19,20 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        Logger.error(`[Auth] Username ${username} already exists`);
+        throw new ConflictException(`Username ${username} already exists`);
+      } else {
+        Logger.error(
+          `[Auth] Internal Server Error while saving user ${username}`,
+        );
+        throw new InternalServerErrorException();
+      }
+    }
 
     return user;
   }
